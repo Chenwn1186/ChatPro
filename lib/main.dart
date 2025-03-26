07fbecc9775a89702886e12807095f0600f4c841
cwn <896535341@qq.com>
2025-03-26 21:49:06 +0800

    添加了切换模型的功能。

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chat_pro/backend.dart';
 import 'package:chat_pro/chat_controller.dart';
 import 'package:chat_pro/chat_list.dart';
 import 'package:chat_pro/ui/pura_multiple_radial_gradients.dart';
@@ -51,53 +52,80 @@ class MainApp extends StatelessWidget {
         home: Stack(
           children: [
             Selector<ChatController, String>(
-            selector: (_, myType) => ChatThemes().currentTheme,
-            shouldRebuild: (previous, next) => true,
-            builder: (context, currentTheme, child) {
-              return PuraMultipleRadialGradients(
-                inputPoints: [
-                  InputPoint(
-                    const Offset(0.25, 0.25),
-                    ChatThemes().getColors()[0].withAlpha(150),
-                    0.25,
-                    0.35,
-                    const Duration(seconds: 2),
-                  ),
-                  InputPoint(
-                    const Offset(0.75, 0.25),
-                    ChatThemes().getColors()[2],
-                    0.28,
-                    0.35,
-                    const Duration(seconds: 3),
-                  ),
-                  InputPoint(
-                    const Offset(0.6, 0.75),
-                    ChatThemes().getColors()[0],
-                    0.26,
-                    0.38,
-                    const Duration(seconds: 3),
-                  ),
-                  InputPoint(
-                    const Offset(0.4, 0.5),
-                    ChatThemes().getColors()[6],
-                    0.12,
-                    0.28,
-                    const Duration(seconds: 2, microseconds: 450),
-                  ),
-                  InputPoint(
-                    const Offset(0.1, 0.8),
-                    ChatThemes().getColors()[6],
-                    0.12,
-                    0.18,
-                    const Duration(seconds: 2, microseconds: 450),
-                  ),
-                ],
-                backgroundColor: Colors.white,
-                blurRadius: 40,
-              );
-            },
-          ),
+              selector: (_, myType) => ChatThemes().currentTheme,
+              shouldRebuild: (previous, next) => true,
+              builder: (context, currentTheme, child) {
+                return PuraMultipleRadialGradients(
+                  inputPoints: [
+                    InputPoint(
+                      const Offset(0.25, 0.25),
+                      ChatThemes().getColors()[0].withAlpha(150),
+                      0.25,
+                      0.35,
+                      const Duration(seconds: 2),
+                    ),
+                    InputPoint(
+                      const Offset(0.75, 0.25),
+                      ChatThemes().getColors()[2],
+                      0.28,
+                      0.35,
+                      const Duration(seconds: 3),
+                    ),
+                    InputPoint(
+                      const Offset(0.6, 0.75),
+                      ChatThemes().getColors()[0],
+                      0.26,
+                      0.38,
+                      const Duration(seconds: 3),
+                    ),
+                    InputPoint(
+                      const Offset(0.4, 0.5),
+                      ChatThemes().getColors()[6],
+                      0.12,
+                      0.28,
+                      const Duration(seconds: 2, microseconds: 450),
+                    ),
+                    InputPoint(
+                      const Offset(0.1, 0.8),
+                      ChatThemes().getColors()[6],
+                      0.12,
+                      0.18,
+                      const Duration(seconds: 2, microseconds: 450),
+                    ),
+                  ],
+                  backgroundColor: Colors.white,
+                  blurRadius: 40,
+                );
+              },
+            ),
             Scaffold(
+              appBar: AppBar(
+                actions: [
+                  PopupMenuButton(
+                      icon: const Icon(Icons.change_circle_rounded),
+                      tooltip: '切换模型',
+                      itemBuilder: (context) {
+                        return OpenAIUserInteraction().getModels().map((e) {
+                          return PopupMenuItem(
+                            value: e,
+                            child: Text(e),
+                            onTap: () {
+                              OpenAIUserInteraction().setModel(e);
+                            },
+                          );
+                        }).toList()..add(
+                            PopupMenuItem(
+                              value: '当前模型',
+                              child: Text('当前模型: ${OpenAIUserInteraction().model}'),
+                              onTap: () {
+                                // OpenAIUserInteraction().setModel('设置');
+                              },
+                            ),
+                          
+                        );
+                      })
+                ],
+              ),
               backgroundColor: const Color.fromARGB(160, 211, 211, 211),
               body: Row(
                 children: [
