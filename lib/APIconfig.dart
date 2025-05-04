148d93bf72d9fdd80a2da8b246ba23098d79841d
YuhengLiu <liuyh357@mail2.sysu.edu.cn>
2025-05-04 18:15:41 +0800

    删除聊天记录

--- lib/APIconfig.dart
+++
@@ -1,8 +0,0 @@
-// class APIConfig {
-//   static const String apiKey =
-//       "sk-lDIxGqCIvASAAMQGFd954108C8D74d3eB4334601D15203Aa";
-//   static const String baseUrl = "https://vip.yi-zhan.top";
-//   static const String backend_apiKey =
-//       "sk-528.kT3wdhoKY531DD59egtWtRZKT8deOwLVo0i0IxorxyQVePoY";
-//   static const String bckend_base_url = "https://wcode.net/api/gpt/v1";
-// }
